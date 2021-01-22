c57fa93e9584f55e0ad46790684983927eef7154
compiler <lukerochedev@gmail.com>
2021-01-22 16:03:44 -0500

    reqs and startup

---
+++ makefile
@@ -0,0 +1,7 @@
+SRC_DIR = src/
+ENTRY_POINT = $(SRC_DIR)core.py
+
+all: main
+
+main:
+	python3 ENTRY_POINT
---
+++ src/core.py
@@ -0,0 +1,4 @@
+GIT_URL_START = https://github.com/trending/java?since=daily&spoken_language_code=en
+GIT_URL_TOPICS_JAVA = https://github.com/topics/java
+
+x = 3
