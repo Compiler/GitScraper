a31fba6f36f6e058e7aafb9749d0d07cc2a4cdf7
compiler <lukerochedev@gmail.com>
2021-02-07 10:42:38 -0500

    repo scraper

--- src/parse_repos.py
+++ src/parse_repos.py
@@ -14,6 +14,9 @@ import traceback
 from lxml.html import fromstring
 import sys, getopt
 
+filename = "ScrapedUsers/GithubRepositories.txt"
+f = open(filename, "a", encoding='utf-8')
+
 def get_proxies():
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
@@ -27,18 +30,22 @@ def get_proxies():
     return prox
 
 
+repos = dict()
 
 def parseContent(htmlData):
     domStruct = BeautifulSoup(htmlData, 'html.parser')
 
     allRepos = domStruct.select('li.col-12.d-flex.width-full.py-4.border-bottom.color-border-secondary.public.source')
-    langs = domStruct.select('ml-0.mr-3')
-    for eachLanguage in langs:
-        print(eachLanguage)
     for eachRepo in allRepos:
-        href = eachRepo.a.attrs["href"]
-        print(href) 
+        href = 'https://github.com' + eachRepo.a.attrs["href"]
+        lang = eachRepo.span
+        if(lang != None):
+            print(lang.text.split()[0],'-',href)
+            repos[lang.text.split()[0]] =  href
+            f.write(lang.text.split()[0]+'\t\t\t\t\t\t' + href+'\n')
+    print('done')
     
+
 def scrapeRepos(startURL):
     try:
         res = requests.get('http://github.com/'+str(startURL)+'?tab=repositories', headers= {'User-Agent' : "Mozilla/5.0"})
@@ -58,21 +65,18 @@ def scrapeRepos(startURL):
         return scrapeRepos(startURL)
 
 
+
 if __name__ == '__main__':
-    filename = "ScrapedUsers/GithubUsernames.txt"
     flag = True
-    startKey = ''
-    f = open(filename, "a", encoding='utf-8')
-    if(startKey == ''):
+    usernameToContinueFrom = ''
+    if(usernameToContinueFrom == ''):
         flag = False
     for i in range(5):
         for username in map(''.join, itertools.product('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_', repeat=i)):
             if(flag):
-                if(username == startKey):
+                if(username == usernameToContinueFrom):
                     flag = False
-                print(username,'x')
+                print('x',end='')
                 continue
-            
-            if(scrapeRepos(username) == 1):
-                f.write('https://github.com/' + username + '\n')
+            scrapeRepos(username)
     f.close()
